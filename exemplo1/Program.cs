4a353860a5fd6030c5661948c729e759eabef3db
Isaac Orcine <103529813+isaacOrcine@users.noreply.github.com>
2024-10-12 00:27:27 -0300

    exemplos de teste

---
+++ exemplo1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<MyObject> objects = new List<MyObject>();
+
+        for (int i = 0; i < 10000; i++)
+        {
+            MyObject obj = new MyObject(i);
+            objects.Add(obj);
+            // Simula o uso de memória sem liberar os objetos
+        }
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
+class MyObject
+{
+    public int Id { get; }
+    public string Data { get; }
+    public MyObject(int id)
+    {
+        Id = id;
+        Data = new string('A', 10000); // Alocação de memória
+    }
+}
---
+++ exemplo2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ComplexObject
+{
+    public int Id { get; }
+    public double[] Data { get; }
+
+    public ComplexObject(int id)
+    {
+        Id = id;
+        Data = new double[10000]; // Aumentando o tamanho do array
+    }
+}
+
+// Código executável
+List<ComplexObject> objects = new List<ComplexObject>();
+
+for (int i = 0; i < 100000; i++) // Aumentando o número de alocações
+{
+    ComplexObject obj = new ComplexObject(i);
+    objects.Add(obj);
+}
+
+Console.WriteLine("Processamento de objetos complexos concluído.");
+
+
